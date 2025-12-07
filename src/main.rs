5fb4e2db7db5ce4087bfca954e2221609e98d77c
Andrey <andrekabatareika@gmail.com>
2025-12-07 17:31:17 +0300

    feat: complete 2025::7

--- src/main.rs
+++ src/main.rs
@@ -78,5 +78,5 @@ macro_rules! year {
 }
 
 year!(year2025 "Year 2025"
-    day01, day02, day03, day04, day05, day06
+    day01, day02, day03, day04, day05, day06, day07
 );
---
+++ src/year2025/day07.rs
@@ -0,0 +1,70 @@
+//! # Laboratories
+type Input = (u64, u64);
+
+pub fn parse(input: &str) -> Input {
+    let lines = input.lines().map(str::as_bytes).collect::<Vec<_>>();
+    let width = lines[0].len();
+    let center = width / 2;
+
+    let mut splits = 0;
+    let mut timelines = vec![0; width];
+    timelines[center] = 1;
+
+    for (y, row) in lines.iter().skip(2).step_by(2).enumerate() {
+        for x in ((center - y)..(center + y + 1)).step_by(2) {
+            let count = timelines[x];
+
+            if count > 0 && row[x] == b'^' {
+                splits += 1;
+                timelines[x] = 0;
+                timelines[x - 1] += count;
+                timelines[x + 1] += count;
+            }
+        }
+    }
+
+    (splits, timelines.iter().sum())
+}
+
+pub fn part1(input: &Input) -> u64 {
+    input.0
+}
+
+pub fn part2(input: &Input) -> u64 {
+    input.1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............";
+
+    #[test]
+    fn part1_test() {
+        let input = parse(EXAMPLE);
+        assert_eq!(part1(&input), 21);
+    }
+
+    #[test]
+    fn part2_test() {
+        let input = parse(EXAMPLE);
+        assert_eq!(part2(&input), 40);
+    }
+}
